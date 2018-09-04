df3e2b621c3b3c278a8548aff5871956d83fed4b
tararico <emipu.uq@gmail.com>
2018-09-04 22:22:05 +0900

    add columns

--- books_app/app/controllers/books_controller.rb
+++ books_app/app/controllers/books_controller.rb
@@ -68,7 +68,7 @@ class BooksController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def book_params
-      params.require(:book).permit(:title, :memo)
+    def book_params      
+      params.require(:book).permit(:title, :memo, :author, :picture)
     end
 end
--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904093841) do
+ActiveRecord::Schema.define(version: 20180904131354) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.string "picture"
   end
 
 end
