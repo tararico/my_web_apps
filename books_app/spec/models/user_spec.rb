b4589bce1def7347c855419f818e2b3d35d185b7
tararico <emipu.uq@gmail.com>
2018-12-15 15:46:45 +0900

    add testcase1

--- books_app/spec/models/user_spec.rb
+++ books_app/spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
       expect(user.errors[:name]).to include("can't be blank")
     end
   end
-  describe "from_omniauth" do
+  describe "find_or_create_for_oauth" do
     context "userが登録されている場合" do
       it "登録されているuserを返すこと" do
         user = User.create(
@@ -17,14 +17,37 @@ RSpec.describe User, type: :model do
           provider: "facebook",
           password: "password"
         )
-        expect(User.from_omniauth(auth).email).to eq("sample@test.com")
+        class MockAuth
+          def initialize(user)
+            @user = user
+          end
+          def provider
+            "facebook"
+          end
+          def uid
+            @user.uid
+          end
+          def info
+            MockInfo.new
+          end
+        end
+        class MockInfo
+          def name
+            "TestUser"
+          end
+          def email
+            "sample@test.com"
+          end
+        end
+        auth = MockAuth.new(user)
+        expect(User.find_or_create_for_oauth(auth).email).to eq("sample@test.com")
       end
     end
     context "userが登録されていない場合" do
-      it "userを登録すること" do
+      xit "userを登録すること" do
         expect().to eq
       end
-      it "登録したuserを返すこと" do
+      xit "登録したuserを返すこと" do
         expect().to eq
       end
     end
