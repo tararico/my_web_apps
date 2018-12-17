85309965e2e5dd08bffd238284ffd35f6d0418c5
tararico <emipu.uq@gmail.com>
2018-12-17 11:37:22 +0900

    delete unused method

--- books_app/spec/models/user_spec.rb
+++ books_app/spec/models/user_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "validation" do
-    it "is invalid without a name" do
-      user = User.new(name: nil)
-      user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
-    end
-  end
   describe "find_or_create_for_oauth" do
     before do
       class MockAuth
